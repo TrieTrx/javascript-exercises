80452781946f788d42062c0145ca706ca5949437
TrieTrx <phandinhtriet2205@gmail.com>
2026-01-20 22:33:33 +0700

    Solved

--- foundations/09_sumAll/sumAll.js
+++ foundations/09_sumAll/sumAll.js
@@ -1,5 +1,8 @@
-const sumAll = function() {
-
+const sumAll = function (left, right) {
+    if (!Number.isInteger(left) || !Number.isInteger(right)) return "ERROR";
+    if (left <= 0 || right <= 0) return "ERROR";
+    if (left > right) [left, right] = [right, left];
+    return (left + right) * (right - left + 1) / 2;
 };
 
 // Do not edit below this line
