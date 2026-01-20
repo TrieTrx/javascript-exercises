4d7a851fb8af803246f528979ceeaf941c4d9026
TrieTrx <phandinhtriet2205@gmail.com>
2026-01-20 22:33:52 +0700

    Solved

--- foundations/08_removeFromArray/removeFromArray.js
+++ foundations/08_removeFromArray/removeFromArray.js
@@ -1,4 +1,5 @@
-const removeFromArray = function() {
+const removeFromArray = function (arr, ...remList) {
+    return arr.filter(item => !remList.includes(item));
 };
 
 // Do not edit below this line
